1fb82fac5b1553c3d88911e328b5da6a2f6f1ea0
unknown <202080469@psu.palawan.edu.ph>
2022-11-30 00:48:29 +0800

    Log-app

---
+++ log-app/config/config.php
@@ -0,0 +1,7 @@
+<?php
+	define('ROOT_URL', 'http:/localhost/logApp-scaling-octo/');
+	define('DB_HOST', 'sql6.freesqldatabase.com');
+	define('DB_USER', 'sql6525706');
+	define('DB_PASS', 'Qxl1CYUFY6');
+	define('DB_NAME', 'sql6525706');
+?>
